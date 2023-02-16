3662acf38670916dfc18b5b1dda6832b8c71e80c
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-16 13:55:25 +0100

    16.02.2023

---
+++ src/main/kotlin/1_Pierwszy program.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+    // \n używamy w stringach gdy chcemy przenieść wyraz do nowej linijki
+    var name: String = "Anonim"
+    print("Podaj imię: ")
+    name = readLine().toString()
+    print("Hello $name!")
+
+}
---
+++ src/main/kotlin/2_Zmienne, typy danych.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>) {
+     /*var liczba: Int
+     var Liczba: Int
+*/
+     // Zmiana literki na dużą powoduje to, że tworzy się druga zmienna
+
+
+    var liczba: Int = 5
+    println(liczba)
+    println("int: " + Int.MIN_VALUE)
+    println("-int: " + Int.MAX_VALUE)
+
+    var byte: Byte
+    println("byte: " + Byte.MAX_VALUE)
+
+    var short: Short
+    println("short: " + Short.MAX_VALUE)
+
+    var long: Long
+    println("long: " + Long.MAX_VALUE)
+
+    var uInt: UInt
+    println(UInt.MIN_VALUE)
+    println(UInt.MAX_VALUE)
+
+    var float: Float = 111.222f
+    println(float)
+    println("float: " + Float.MAX_VALUE)
+
+}
