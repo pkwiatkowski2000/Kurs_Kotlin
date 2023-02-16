83a66bb2101932cc112f8fc4907f7778bbd88b62
Paweł <pawel.kwiatkowski2000@gmail.com>
2023-02-16 21:20:41 +0100

    16.02.2023

---
+++ src/main/kotlin/4_Operatory porównania.kt
@@ -0,0 +1,19 @@
+fun main (args: Array<String>) {
+
+  var a = 5
+  var b = 3
+  var x: Boolean
+
+  x = a == b
+  println(x) // A jest równe b
+
+  println(a == b) // A jest równe b
+  println(a != b) // A jest różne od b
+
+  println( a > b) // A jest większe od b
+  println ( a < b) // A jest mniejsze od b
+
+  println(a >= b) // A jest większe bądź równe b
+  println(a <= b) // A jest mniejsze bądź równe b
+
+}
