bf853fbcab4e8fed984a199faa8e36381088403e
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-17 14:42:49 +0100

    17.02.2023

--- src/main/kotlin/12_Praktyka - Zgadnij liczbę.kt
+++ src/main/kotlin/12_Praktyka - Zgadnij liczbę.kt
@@ -1,3 +1,35 @@
-fun main (args: Array<String>) {
+import kotlin.random.Random
 
+/*
+fun main (args: Array<String>) {
+    for (i in 1..30) {
+        println(Random.nextInt(1, 11))
+
+        println((1.. 10).random())
+    }
+}*/
+
+
+fun main (args: Array<String>) {
+    val x = Random.nextInt(1, 11)
+    var a: Int
+    var i = 0
+
+    println("Zgadnij liczbę z przedziału od 1 do 10")
+
+    do {
+        i++
+        print("Podaj liczbę: ")
+        a = readLine()!!.toInt()
+
+        if (x > a) {
+            println(" Niestety, wylosowana liczba jest większa od Twojej")
+        }
+        else if (x< a) {
+            println(" Niestety, wylosowana liczba jest mniejsza od Twojej")
+        }
+
+    } while (x != a)
+
+    println("Brawo, zgadłeś liczbę za $i razem")
 }
