ea08756166c724cf6245ab95f0afd61961c777ce
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-17 08:21:46 +0100

    17.02.2023

--- src/main/kotlin/5_Operatory logiczne.kt
+++ src/main/kotlin/5_Operatory logiczne.kt
@@ -1,4 +1,17 @@
 fun main (args: Array<String>) {
 
-    
+     var x: Boolean
+     x = 5 > 5 || 1 != 2
+
+     println(x)
+
+     println(true || false) // || - LUB - suma, ustępuje pierwszeństwa mnożenia
+     println(false && true) // && - I - iloczyn mnożenia, pierwszeństwo w działaniu nad dodawaniem
+     println(!true) // ! - NIE
+
+     println((2 + 2) *2)
+     println(true || false && false)
+     println((true || false) && false)
+     println((true || false) && !false)
+
 }
