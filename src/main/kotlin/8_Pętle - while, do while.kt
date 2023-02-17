113c5422372c76d88065776eeffb0f3ed9e80c5a
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-17 10:55:03 +0100

    17.02.2023

--- src/main/kotlin/8_Pętle - while, do while.kt
+++ src/main/kotlin/8_Pętle - while, do while.kt
@@ -1,12 +1,19 @@
 fun main(args: Array <String>) {
 
-    var i = 0
+    var i = 100
 
     while (i < 10) {
         println("i: $i")
         i++
     }
 
-    println(i)
+    var j = 100
 
+    do {
+        println("j: $j")
+        j++
+    } while (j < 10)
+
+
+    // Pętla do while różni się od while tym, że przynajmniej raz zostanie wykonana
 }
---
+++ src/main/kotlin/9_Instrukcje skoku - break, continue.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    var i = 0
+
+    while (i < 10 || true) {
+        if (i >=10)
+            break // przerywa działanie
+        println("i: $i")
+        i++
+    }
+
+    println("--------------------------------------------------")
+
+    var j = 0
+
+    while (j < 20) {
+        j++
+        if(j % 2 == 1) {
+            continue // przechodzi do kolejnego kroku pętli od miejsca continue
+        }
+        println("j: $j")
+        }
+
+
+    }
