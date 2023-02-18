0e3eeb08f6e2d059cc56761401b91d1795549e05
Paweł <pawel.kwiatkowski2000@gmail.com>
2023-02-18 15:32:57 +0100

    18.02.2023

--- src/main/kotlin/16_ Funkcje wyższego rzędu.kt
+++ src/main/kotlin/16_ Funkcje wyższego rzędu.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>) {
+    var sum = add(5, 10) {x -> "Suma = $x"}
+    println(sum)
 
+    sum = add(6, 7, fun(x): String {return "Add = $x"})
+    println(sum)
+
+    println(add(10, 22){x, y, z -> "$x + $y = $z"})
+}
+
+fun add(a: Int, b: Int, function: (Int) -> (String)) : String {
+    return function(a + b)
+}
+
+fun add(a: Int, b: Int, function: (Int, Int, Int) -> String): String {
+    return function(a, b, a + b)
 }
