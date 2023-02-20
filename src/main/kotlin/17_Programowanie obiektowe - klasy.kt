9553f514ed44dc8aa034d801b3306e4931014a55
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-20 08:50:54 +0100

    20.02.2023

--- src/main/kotlin/17_Programowanie obiektowe - klasy.kt
+++ src/main/kotlin/17_Programowanie obiektowe - klasy.kt
@@ -1,4 +1,24 @@
 fun main(args: Array<String>) {
 
-    class Person
+    var p1 = Person()
+    p1.name = "Paweł"
+    p1.age = 23
+    println(p1.name)
+    println(p1.age)
+    p1.print()
+
+    var p2 = Person()
+    p2.name = "Adam"
+    p2.age = 18
+    p2.print()
 }
+
+    class Person {
+        var name = ""
+        var age = 0
+
+        fun print() {
+            println("Imię: $name, wiek: $age.")
+        }
+
+    }
