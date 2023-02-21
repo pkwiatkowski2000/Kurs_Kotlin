783a746c49a558a833cb42f2dfcc46f250ea5f7b
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-21 10:42:41 +0100

    21.02.2023

--- src/main/kotlin/20_Programowanie obiektowe - dziedziczenie.kt
+++ src/main/kotlin/20_Programowanie obiektowe - dziedziczenie.kt
@@ -1,19 +1,29 @@
 fun main(args: Array<String>) {
 // Aby mieć na czym dziedziczyć potrzebujemy co najmniej 2 klasy
 
-    var animal = Animal()
-    var cat = Cat()
+    var animal = Animal("Felix")
+    var cat = Cat("Milka", "White")
     cat.name = "Milka"
     cat.print()
 }
 
- open class Animal{
-    var name = ""
-     fun print() {
+ open class Animal {
+     constructor(name: String) {
+         this.name = name
+     }
+    var name: String
+     open fun print() {
          println("My name is $name")
      }
 }
 
-class Cat : Animal() {
-    var color = ""
+open class Cat: Animal {
+    constructor(name: String, color: String) : super(name) {
+        this.color = color
+    }
+    override fun print() {
+        super.print()
+        println("My color is $color")
+    }
+    var color: String
 }
