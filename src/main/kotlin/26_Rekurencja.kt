2f38e83d59c10eb8e9538154b4262b286fce10c9
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-21 15:02:37 +0100

    21.02.2023

--- src/main/kotlin/26_Rekurencja.kt
+++ src/main/kotlin/26_Rekurencja.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
-    
+
+    // Rekurencja to sytuacja taką, w której funkcja wewnątrz siebie wywołuje samą siebie
+
+    println("0! = " + silnia(0))
+    println("1! = " + silnia(1))
+    println("2! = " + silnia(2))
+    println("3! = " + silnia(3))
+    println("4! = " + silnia(4))
+    println("5! = " + silnia(5))
+}
+fun silnia(n: Int): Int {
+    if (n == 0) {
+        return 1
+    } else {
+        // n! = n * (n-1)!
+        return n* silnia(n-1)
+    }
+
 }
