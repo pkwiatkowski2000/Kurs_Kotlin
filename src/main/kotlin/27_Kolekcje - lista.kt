12b725df767d19d8544b1654e7e513e342b95a07
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-21 15:37:22 +0100

    21.02.2023

--- src/main/kotlin/27_Kolekcje - lista.kt
+++ src/main/kotlin/27_Kolekcje - lista.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
-    
+
+//    var array = listOf(1, 5, 8, 9)
+
+    var list = mutableListOf(1, 5, 8, 9)
+    println("Size: " + list.size)
+    list.add(20)
+    list.add(0, 11)
+    println("Size: " + list.size)
+    list.remove(5)
+    list.removeAt(0)
+    list.removeLast()
+    list.clear()
+    println("Size: " + list.size)
+
+    for (i in list) {
+        println(i)
+    }
+
 }
