7c2a812d8c9d9cb2b814bd7bd3c6ea4f14cf377b
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-22 15:37:02 +0100

    22.02.2023

--- src/main/kotlin/32_Przeciążenia operatorów.kt
+++ src/main/kotlin/32_Przeciążenia operatorów.kt
@@ -1,19 +1,47 @@
+import kotlin.math.sqrt
+
 fun main(args: Array<String>) {
 
-        var p1 = Punkt(2, 7)
-        var p2 = Punkt(1, 5)
-        println("P1 = $p1")
-        println("P2 = $p2")
-        println(p1 == p2)
-        println(p1 + p2)
-    }
+    var p1 = Punkt(2, 7)
+    var p2 = Punkt(1, 5)
+    println("P1 = $p1")
+    println("P2 = $p2")
+    println(p1 == p2)
+    println(p1 + p2)
+    println(p1 + 5)
+    println(p1 + "5")
+    println(p1 * p2)
+    println(p1 * 3)
+    println(p1 * "ABC")
+    println(p1.odległość())
+    println(p2.odległość())
+}
+
 
     class Punkt(x: Int, y: Int) {
         var x = x
         var y = y
 
-        operator fun plus(other: Punkt): Punkt {
-            return Punkt(this.x + other.x, this.y + other.y)
+        fun odległość(): Double {
+            return sqrt(x.toDouble() * x.toDouble() + y.toDouble() * y.toDouble())
+        }
+
+        open operator fun plus(other: Any): Punkt? {
+            if (other is Punkt)
+                return Punkt(this.x + other.x, this.y + other.y)
+            else if (other is Int)
+                return Punkt(this.x + other, this.y + other)
+            else
+                return null
+        }
+
+        operator fun times(other: Any) : Punkt? {
+            if (other is Punkt)
+                return Punkt(this.x * other.x, this.y * other.y)
+            else if (other is Int)
+                return Punkt(this.x * other, this.y * other)
+            else
+                return null
         }
 
         override fun equals(other: Any?): Boolean {
@@ -29,3 +57,5 @@ fun main(args: Array<String>) {
         }
 
     }
+
+24:15 dokończyć
