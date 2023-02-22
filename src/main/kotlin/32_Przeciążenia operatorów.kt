bab5abd07968482b8df0d1e96ea2f3a9d5f7a988
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-02-22 13:38:18 +0100

    22.02.2023

---
+++ src/main/kotlin/32_Przeciążenia operatorów.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+    fun main(args:Array<String>) {
+        var p1 = Punkt(2, 5)
+        println(p1)
+        var p2 = Punkt(2, 5)
+        println(p2.toString())
+        println(p1 == p2)
+        println(p1.equals(p2))
+    }
+
+    class Punkt(x: Int, y: Int) {
+        var x = x
+        var y = y
+
+        override fun equals(other: Any?): Boolean {
+            if (other is Punkt) {
+                return (other.x == this.x && other.y == this.y)
+            } else {
+                return false
+            }
+        }
+
+        override fun toString(): String {
+            return "x: $x, y: $y"
+        }
+
+    }
+
+}
